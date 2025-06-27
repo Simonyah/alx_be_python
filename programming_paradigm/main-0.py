1d1e283615a5dcf23d64b6aa4038e11fff20230b
Simonyah <163657794+Simonyah@users.noreply.github.com>
2025-06-28 05:45:43 +0900

    Update main-0.py
    fix main

--- programming_paradigm/main-0.py
+++ programming_paradigm/main-0.py
@@ -2,7 +2,7 @@ import sys
 from bank_account import BankAccount
 
 def main():
-    account = BankAccount(100)  # You can change starting balance here
+    account = BankAccount(100)  # Starting balance
 
     if len(sys.argv) < 2:
         print("Usage: python main-0.py <command>:<amount>")
